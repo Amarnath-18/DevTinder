4c3a46988614f9b692ab6163383e61ffe67d512a
Amarnath Garai <amarnathgarai2004@gmail.com>
2025-03-31 14:19:40 +0530

    env added for hiding importent data

--- src/config/dataBase.js
+++ src/config/dataBase.js
@@ -1,15 +1,15 @@
 const mongoose = require('mongoose');
-
+require('dotenv').config(); 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb+srv://amarnathgarai2004:8UkRKGi4T5YjKZGU@cluster0.ai6ao.mongodb.net/DevTinder', {
+        await mongoose.connect(process.env.SECRET_KEY, {
             useNewUrlParser: true,
             useUnifiedTopology: true
         });
         console.log('✅ MongoDB Connected Successfully');
     } catch (error) {
         console.error('❌ MongoDB Connection Failed:', error.message);
-        process.exit(1); // Stop the app if DB connection fails
+        process.exit(1);
     }
 };
 
